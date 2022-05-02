ab8a11bef4923506b661f6bb820e8369e2bd8bd5
lakshita1111 <104756373+lakshita1111@users.noreply.github.com>
2022-05-02 13:16:41 +0530

    Add files via upload

---
+++ Student_Project.py
@@ -0,0 +1,13 @@
+import turtle
+
+for i in range(2):
+    turtle.shape("")#Write the shape as square
+    turtle.pencolor("")#Write the pen color as red
+    turtle.forward(200)
+    turtle.left(90)
+    
+for i in range(2):
+    turtle.shape("")#Write the shape as circle
+    turtle.pencolor("")#Write the pen color as green
+    turtle.forward(200)
+    turtle.left(90)
